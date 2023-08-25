cc6f7b4cc89e7954c287bef0f83598c0df582f18
Thales Emerich <thales@lafine.com.br>
2023-08-25 10:06:11 -0300

    Criado mecanismo de paginação

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
             .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
 
     return fetch(url)
